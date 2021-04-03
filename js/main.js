542663559da9d13a0036fc48504324322648cdf9
KevinMorocho <65823017+KevinMorocho@users.noreply.github.com>
2021-04-03 00:12:57 -0500

    Nav/Delete Agregado

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const btnDelete=document.querySelectorAll('btn-delete')
+
+if(btnDelte) {
+    const btnA=Array.from(btnDelete);
+    btnArray.forEach((btn) => {
+        btn.addEventListener('click',(e) => {
+            if(!confirm('¿Seguro quiere eliminar?')) {
+                e.preventDefault();
+            }
+        });
+    });
+}
